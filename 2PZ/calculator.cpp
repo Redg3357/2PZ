3c484d78a411ab7892c6058769171ab8f93467df
Prigunok <voropanov_kirya@mail.ru>
2024-11-19 17:04:51 +0300

    add xor function and test for it

--- 2PZ/calculator.cpp
+++ 2PZ/calculator.cpp
@@ -6,5 +6,6 @@ namespace operations {
 	int subtract(int a, int b) { return a - b; }
 	int multiply(int a, int b) { return a * b; }
 	int divide(int a, int b) { return (b != 0) ? a / b : 0; }
+	int dec_xor(int a, int b) { return a ^ b; }
 	//some changes
 }
