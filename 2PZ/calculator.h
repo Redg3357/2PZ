d695570ae6e0c241e092d1c1c92765c3e21b4d5c
Prigunok <voropanov_kirya@mail.ru>
2024-11-21 14:35:50 +0300

    add dox comments

--- 2PZ/calculator.h
+++ 2PZ/calculator.h
@@ -1,12 +1,17 @@
 #pragma once
 
+
+//! \breidf 
 namespace operations {
-
+	//! \breif add func
 	int add(int a, int b);
+	//! \breif subtract func
 	int subtract(int a, int b);
+	//! \breif multiply func
 	int multiply(int a, int b);
+	//! \breif divide func
 	int divide(int a, int b);
+	//! \breif dec_xor func
 	int dec_xor(int a, int b);
-
 }
 
--- 2PZTEST/test.cpp
+++ 2PZTEST/test.cpp
@@ -1,30 +1,30 @@
 #include "pch.h"
 #include "../2PZ/calculator.cpp"
 
-
+//! \breif tests for add func
 TEST(CalculatorTests, Add) {
     EXPECT_EQ(operations::add(2, 3), 5);
     EXPECT_EQ(operations::add(-1, 1), 0);
 }
-
+//! \breif tests for subtract func
 TEST(CalculatorTests, Subtract) {
     EXPECT_EQ(operations::subtract(5, 3), 2);
     EXPECT_EQ(operations::subtract(-1, -1), 0);
     EXPECT_EQ(operations::subtract(0, 0), 0);
 }
-
+//! \breif tests for multiply func
 TEST(CalculatorTests, Multiply) {
     EXPECT_EQ(operations::multiply(4, 3), 12);
     EXPECT_EQ(operations::multiply(-2, 2), -4);
     EXPECT_EQ(operations::multiply(2, 2), 4);
 }
-
+//! \breif tests for divide func
 TEST(CalculatorTests, Divide) {
     EXPECT_EQ(operations::divide(6, 3), 2);
     EXPECT_EQ(operations::divide(5, 0), 0);
     EXPECT_EQ(operations::divide(-18, 3), -6);
 }
-
+//! \breif tests for xor func
 TEST(CalculatorTests, Xor) {
     EXPECT_EQ(operations::dec_xor(10, 10), 0);
     EXPECT_EQ(operations::dec_xor(107, 63), 84);
